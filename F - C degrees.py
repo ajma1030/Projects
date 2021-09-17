df9db115589780f6c789e9826e33cc01dc721a34
Adolfo M <ajma1030>
2021-09-17 11:42:19 -0400

    temperature calculator

---
+++ F - C degrees.py
@@ -0,0 +1,10 @@
+tempF = float(input("What is the temperature in degrees Fahrenheit?:"))
+
+tempC = ((tempF - (float(32)))/1.8)
+print("The temperature in Celsius is", tempC)
+
+
+temp_C = (float(input("Now what's the temp in Celsius?:")))
+
+temp_F = (temp_C * 1.8) + (float(32))
+print ("The temperature in Fahrenhit is", temp_F)
