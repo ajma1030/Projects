909d5bc34cf3861cd550254929b13e8a6a942c3b
Adolfo M <ajma1030>
2021-09-20 14:15:43 -0400

    conversion code.

---
+++ Lab 1.py
@@ -0,0 +1,13 @@
+#Adolfo Martinez,
+#CS151-05 Prof. Mehri
+#9/20/21
+#Lab 1
+
+
+
+user_mL = float(input("How many milliliters?:"))
+
+teaSpoons = (user_mL/4.929)
+tableSpoons = (user_mL/14.787)
+
+print(user_mL, "mL in teaspoons is:", teaSpoons, "and it is:", tableSpoons, "in tablespoons.")
