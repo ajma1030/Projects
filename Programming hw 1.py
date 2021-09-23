0244a2315fea1ea6cd634ed9b27b5178d480197c
Adolfo M <ajma1030>
2021-09-23 14:11:40 -0400

    programming assignment 1

--- Programming hw 1.py
+++ Programming hw 1.py
@@ -1,6 +1,6 @@
 #Adolfo Martinez
 #CS151-05 Prof. Mehri
-#9/22/21
+#9/23/21
 #Programming assignment: 1
 #This program will tell total area of the walls and ceiling of a room and the necessary amount of paint and primer to cover the room
 #Input: Room dimensions
@@ -12,7 +12,7 @@ wall_h = int(input("Finally what is the height of your wall in feet?:"))
 
 t_area = (((wall_l * wall_h)*4)+(wall_l**2))
 
-primer_need = ((t_area//200)+1)
-paint_need = ((t_area//350)+1)
+primer_need = ((t_area//200))
+paint_need = ((t_area//350))
 
 print("The total area you're going to need to paint is", t_area, "sq.ft and you will need", primer_need, "gallons of primer and", paint_need, "gallons of paint.")
